c2450deeb0d33021178f0fe750a4cd3a1037863b
Aaron Nagucki <aaron.nagucki@gmail.com>
2015-12-30 13:22:05 -0500

    added mailer for posts

--- app/mailers/favorite_mailer.rb
+++ app/mailers/favorite_mailer.rb
@@ -13,4 +13,12 @@ class FavoriteMailer < ApplicationMailer
  # #19
     mail(to: user.email, subject: "New comment on #{post.title}")
   end
+
+  def new_post(post)
+    headers["Message-ID"] = "<posts/#{post.id}@blooming-caverns-5675.example>"
+    headers["In-Reply-To"] = "<post/#{post.id}@your-app-name.example>"
+    headers["References"] = "<post/#{post.id}@your-app-name.example>"
+
+    @post = post
+    mail(to: post.user.email, subject: "You're now following #{post.title}")
 end
